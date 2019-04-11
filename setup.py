e88593e96d92377d28dcc1bbf09a4bc4cd31f582
jdrtommey <jake.tommey@gmail.com>
2019-04-11 11:03:02 +0100

    initial files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+   name='ryd',
+   version='0.1(dev)',
+   description='Calculate properties of rydberg atoms and generate their hamiltonians in external fields',
+   author='J.D.R Tommey',
+   author_email='ucapdrt[at]ucl[dot]ac[dot]uk',
+   url="https://github.com/jdrtommey/rydprop",
+   packages=find_packages(),  #same as name
+   install_requires=['numpy','scipy','numba','tqdm'], #external packages as dependencies
+)
